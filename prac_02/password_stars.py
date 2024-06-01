50d7735cc4237eaee5002c394e065cb33e5249dd
Sithuhein <sithuhein@my.jcu.edu.au>
2024-06-01 21:48:24 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,19 @@
 def main():
+    get_password()
+
+
+def get_password():
     password = input("Type the password: ")
     MINIMUM_LENGTH = 7
     while len(password) < MINIMUM_LENGTH:
         print("Invalid password")
         password = input("Type the password: ")
+    the_password(password)
+
+
+def the_password(password):
     asterisks = len(password) * "*"
     print(asterisks)
+
 
 main()
