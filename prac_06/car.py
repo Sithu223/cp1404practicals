6fd195937fff347fce7e40e152a6e4dc1d0cd93c
Sithuhein <sithuhein@my.jcu.edu.au>
2024-07-13 15:53:30 +0800

    added name

--- prac_06/car.py
+++ prac_06/car.py
@@ -5,7 +5,7 @@ Name = Sithu Hein
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, name, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
