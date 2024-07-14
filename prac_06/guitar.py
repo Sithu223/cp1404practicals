c9ab8b3d542e9d4da4cbb0253d812d1cb7866f61
Sithuhein <sithuhein@my.jcu.edu.au>
2024-07-14 21:41:34 +0800

    adding class

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+"""
+Estimate = 30 minutes
+Actual =
+"""
+
+CURRENT_YEAR = 2022
+VINTAGE_AGE = 50
+
+class Guitar:
+    """Represent information about guitar."""
+    def __init__(self, name, year=0, cost=0):
+        """Create a Guitar from the given values"""
+        self.name = name
+        self.year = year
+        self.cost = cost
