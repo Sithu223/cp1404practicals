f015a8d14299f2e76d32cc61f4ec30414a0f715f
Sithuhein <sithuhein@my.jcu.edu.au>
2024-07-14 21:11:26 +0800

    finish the code

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,6 +1,6 @@
 """
-Estimate = 20
-Actual =
+Estimate = 20 minutes
+Actual = 17 minutes
 """
 
 from prac_06.programming_language import ProgrammingLanguage
@@ -9,3 +9,8 @@ python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 print(python)
+programming_language = [python, ruby, visual_basic]
+print("The dynamically typed languages are: ")
+for programming_language in programming_language:
+    if programming_language.is_dynamic():
+        print(programming_language.language_name)
