2b658cc7d6cc4b4dac6af08d95a9285ad6ffdc24
Sithuhein <sithuhein@my.jcu.edu.au>
2024-07-21 22:16:16 +0800

    adding class for project

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,3 +3,13 @@ Estimate = 20 minutes
 Actual =
 """
 
+class Project:
+    """Represent information about a project."""
+    def __init__(self, name, start_date, priority=0, cost_estimate=0.0, completion_percentage=0):
+        """Construct a Project from the given values."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
