4d7e1b9b7e35e87cb48111d347bf40bdd09c39d1
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-01 21:47:48 +0800

    Copy and remain the two files

---
+++ prac_02/score.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 - Practical
+"""
+
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Your score is invalid!")
+        score = float(input("Enter score: "))
+
+    result = determine_score(score)
+    print(result)
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}")
+    random_result = determine_score(random_score)
+    print(random_result)
+
+def determine_score(score):
+    if score >= 90:
+        return "Excellent!"
+    elif score >= 50:
+        return "Passable!"
+    else:
+        return "Bad!"
+
+main()
---
+++ prac_02/temperatures.py
@@ -0,0 +1,33 @@
+"""
+CP1404/CP5632 - Practical
+"""
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            convert_celsius_to_fahrenheit = get_fahrenheit(celsius)
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            convert_fahrenheit_to_celsius = get_celsius(fahrenheit)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def get_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print(f"Result: {fahrenheit:.2f} F")
+    return get_fahrenheit
+
+def get_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(f"Result: {celsius:.2f} C")
+    return get_celsius
+
+main()
