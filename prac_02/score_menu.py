54dc734ce1eb04f3d1682101419eeeaa280fa508
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-01 22:10:40 +0800

    Creating while loop to check the iteration

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,6 +1,16 @@
+from prac_02.score import determine_score
+
 MENU = "G - Get a valid score\nP - Print result\nS - Show stars\nQ - Quit"
 
 def main():
     print(MENU)
     choice = input(">>> ").upper()
     score = float(input("Enter score: "))
+    while choice != "Q":
+        if choice == "G":
+            determine_score(score)
+        elif choice == "S":
+            show_stars(score)
+        else:
+            print("Invalid choice")
+        print(MENU)
