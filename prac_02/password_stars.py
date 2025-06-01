68e4bae2930c5fa0f100f3387b230ee1c09b412c
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-01 22:44:17 +0800

    Add get_password by refactoring

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,16 @@
 MINIMUM_LENGTH = 8
 def main():
-    password = input("Enter your password: ")
+    password = get_password()
 
+
+def get_password():
+    password = input("Enter your password: ")
     while len(password) < MINIMUM_LENGTH:
         print(f"Password must be at least {MINIMUM_LENGTH} characters.")
         password = input("Enter your password: ")
+        return password
+
     print("*" * len(password))
+
 
 main()
