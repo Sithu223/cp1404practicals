8038ec9dc42e866c951a4306cafb1cb81b6597d4
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-08 19:08:04 +0800

    Complete first and second question after testing the code in console

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# What did you see on line 1?
+# I see 8 as the output.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 8 and the largest number is 14.
+
+# What did you see on line 2?
+# I see the first output as 9.
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 3 and the largest number is 9.
+# Could line 2 have produced a 4?
+# No, It cannot produce a 4.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+#
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
