037e691699bc68fca248eaaf711be88ed52ffce4
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-08 19:11:33 +0800

    Complete the final question and final code

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -16,3 +16,7 @@
 # The smallest number is 3.6554952081100414 and the largest number is 4.851705759619277.
 
 # Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+
+number = random.randint(1, 100)
+print(number)
