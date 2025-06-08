5e61bd85485bb95a43524c702bd63565fe0cdcf8
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-08 20:15:48 +0800

    Final code with the fourth question

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,17 +1,25 @@
-# # 1
-# FILENAME = "name.txt"
-# name = input("Enter name: ")
-# out_file = open(FILENAME, "w")
-# out_file.write(name)
-# out_file.close()
-# # 2
-# in_file = open(FILENAME, "r")
-# text = in_file.read()
-# print(f"Your name is {text}")
-# in_file.close()
+# 1
+FILENAME = "name.txt"
+name = input("Enter name: ")
+out_file = open(FILENAME, "w")
+out_file.write(name)
+out_file.close()
+# 2
+in_file = open(FILENAME, "r")
+text = in_file.read()
+print(f"Your name is {text}")
+in_file.close()
 
 # 3
 in_file = open("numbers.txt", "r")
 line_1 = int(in_file.readline().strip())
 line_2 = int(in_file.readline().strip())
 print(f"{line_1 + line_2}")
+
+# 4
+total = 0
+with open("numbers.txt", "r") as in_file:
+    for line in in_file:
+        number = int(line)
+        total += number
+print(total)
