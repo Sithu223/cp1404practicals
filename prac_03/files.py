331c14720e732ac015ff750032523728a3074621
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-08 20:05:46 +0800

    Complete second question

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,10 @@ name = input("Enter name: ")
 out_file = open(FILENAME, "w")
 out_file.write(name)
 out_file.close()
+# 2
+in_file = open(FILENAME, "r")
+text = in_file.read()
+print(f"Your name is {text}")
+in_file.close()
+
+
