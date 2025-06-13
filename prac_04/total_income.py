62c4e661cb63a29c5c215bc18c1a3758e5ef0868
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-13 16:07:53 +0800

    Change the string concatenation to f-string format for income input

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,12 +5,12 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of number_of_months."""
+    """Display income report for incomes over a given number of months."""
     incomes = []
-    number_of_months = int(input("How many number_of_months? "))
+    number_of_months = int(input("How many months? "))
 
     for month in range(1, number_of_months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
