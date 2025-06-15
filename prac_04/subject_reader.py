531a56e6ba8aa4afb74a71b1677ba5eae0fa1cb6
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-15 17:04:21 +0800

    Append subjects to the list to get cumulative nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -23,8 +23,10 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        subjects.append(parts)
+        print(subjects)
         print("----------")
     input_file.close()
-
+    return subjects
 
 main()
