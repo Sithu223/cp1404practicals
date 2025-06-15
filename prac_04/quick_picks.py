cecce8f96ac6d3fe4410b6b28ceac25b97d623e2
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-15 18:31:56 +0800

    Add main function structure of the program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,4 +1,13 @@
 import random
+
+from kivy.input.providers.probesysfs import get_inputs
+
 MIN_VALUE = 1
 MAX_VALUE = 45
 NUMBERS_PER_LINE = 6
+
+def main():
+    number_of_quick_picks = get_input()
+    for i in range(number_of_quick_picks):
+        quick_picks = generate_quick_picks()
+        display_quick_picks(quick_picks)
