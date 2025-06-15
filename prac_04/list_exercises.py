f5e161d5ce2769c739c14681ffea069d39daf885
SithuHein <sithuhein@my.jcu.edu.au>
2025-06-15 17:25:12 +0800

    Ask the user input to enter their username

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,4 @@ print(f"The average of the numbers is {sum(numbers)/len(numbers)}.")
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
+username = input("Enter the username: ")
