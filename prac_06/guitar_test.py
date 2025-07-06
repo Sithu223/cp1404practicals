5cc11f4ad7909573f42f6197eb852b58484b276a
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-06 17:33:09 +0800

    Add get_age() tests with expected vs actual output

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -8,3 +8,7 @@ from prac_06.guitar import Guitar
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.40
+guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
+guitar2 = Guitar("Another Guitar", 2013, 2506.80)
+print(f"{guitar1.name} get_age() - Expected 100. Got {guitar1.get_age()}")
+print(f"{guitar2.name} get_age() - Expected 100. Got {guitar2.get_age()}")
