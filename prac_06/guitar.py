dd5091e6584a76caa2a590fed2ea68e4b2266e4b
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-06 17:08:07 +0800

    Add constants and Guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,3 +2,14 @@
 Estimate = 30 minutes
 Actual =
 """
+
+CURRENT_YEAR = 2025
+VINTAGE_YEAR = 50
+
+class Guitar:
+    """Represent information about guitar."""
+    def __init__(self, name, year = 0, cost = 0):
+        """Create a Guitar from given values"""
+        self.name = name
+        self.year = year
+        self.cost = cost
