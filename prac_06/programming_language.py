840db3da2fdf26f65f9c497cce6ef8538428a447
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-06 16:25:45 +0800

    Define ProgrammingLanguage class and its __init__

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -2,3 +2,12 @@
 Estimate = 15 minutes
 Actual =
 """
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+    def __init__(self, language_name, typing, reflection, year=0):
+        """Create a ProgrammingLanguage from given values."""
+        self.language_name = language_name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
