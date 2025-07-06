b95587a32015de8a112597bbc15efe97a2ebd4e8
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-06 18:00:24 +0800

    Ask user input for name again and Insert hard-coded sample guitars for testing

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -14,3 +14,7 @@ def main():
         guitars.append(guitar_to_add)
         print(guitars)
         print(f"{guitar_to_add}, added.")
+        name = input("Name: ").title()
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.90))
