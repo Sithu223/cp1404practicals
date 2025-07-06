f5191e51c27d912f1f3996d42b274f934e716d24
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-06 17:11:03 +0800

    Add __str__ and __repr__ for formatted guitar description

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -13,3 +13,11 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def __repr__(self):
+        """Return string representation of a Guitar."""
+        return f"{self.name} ({self.year}) : ${self.cost}"
