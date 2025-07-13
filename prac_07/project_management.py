1183785137a8666d651bae52b3b89f7f1564d061
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-13 21:21:54 +0800

    Import datetime and MENU

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,3 +2,11 @@
 Estimate = 20 minutes
 Actual =
 """
+
+import datetime
+from prac_07.project import Project
+
+MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n(A)dd new projects\n(U)pdate"\
+        "project\n(Q)uit")
+FILENAME = "projects.txt"
+projects = []
