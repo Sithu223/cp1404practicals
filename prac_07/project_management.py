0a690a7f510da83d5c1952f1fa419bf45ab2a523
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-13 21:26:15 +0800

    Add the main function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -10,3 +10,25 @@ MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects
         "project\n(Q)uit")
 FILENAME = "projects.txt"
 projects = []
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            load_projects(FILENAME)
+        elif choice == "S":
+            save_projects(FILENAME)
+        elif choice == "D":
+            display_projects()
+        elif choice == "F":
+            filter_projects()
+        elif choice == "A":
+            add_projects()
+        elif choice == "U":
+            update_projects()
+        else:
+            print("Invalid input")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you for using custom-built project management software.")
