295439c53067055f3c7e502ec8b3b3041a02f421
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-13 21:27:41 +0800

    Add save_projects function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -41,3 +41,11 @@ def load_projects(filename):
             name, start_date, priority, cost_estimate, completion = line.strip().split("\t")
             start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
             projects.append(Project(name, start_date, int(priority), float(cost_estimate), int(completion)))
+
+def save_projects(filename):
+    """Save update projects into projects.txt"""
+    with open(filename, 'w') as out_file:
+        out_file.write("Name\tState Date\tPriority\tEstimate\tCompletion\n")
+        for project in projects:
+            out_file.write(f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}\t"
+                           f"{project.cost_estimate}\t{project.completion_percentage}\n"))
