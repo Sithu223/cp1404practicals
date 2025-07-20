9327e1917bb02155bfd8a4c28b929d9bf51ac540
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-20 21:21:48 +0800

    Implement build() to load convert_miles_km.kv and set self.title

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,5 +1,7 @@
-from kivy.app import App
 
+
+from kivy.app import App
+from kivy.lang import Builder
 
 __author__ = 'Sithu Hein'
 
@@ -8,3 +10,8 @@ MILES_TO_KM = 1.60934
 
 class MilesConverterApp(App):
     """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
