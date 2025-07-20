8ad439e907ec867852b0d5eeab15300a02eded32
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-20 21:20:38 +0800

    Create MilesConverterApp class, set up basic Kivy App structure and author metadata.

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+
+
+__author__ = 'Sithu Hein'
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
