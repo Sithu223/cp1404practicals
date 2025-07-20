c66763ddbc503ea91f213b7a5c0f0841820c33f0
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-20 21:02:36 +0800

    Change the window size from 200 to 250 to get the full display

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -8,14 +8,14 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Sithu Hein'
 
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (250, 100)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
