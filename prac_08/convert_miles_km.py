5bc991bf0a58ac47d9f495cb73da876b70e331fd
SithuHein <sithuhein@my.jcu.edu.au>
2025-07-20 21:22:31 +0800

    Write handle_calculate() to read input, convert using MILES_TO_KM, and display result

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -15,3 +15,9 @@ class MilesConverterApp(App):
         """ build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
+
+    def handle_calculate(self):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
