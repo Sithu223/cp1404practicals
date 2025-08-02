b28554cde60bc7885587a82a1b6029fa6d3c943f
SithuHein <sithuhein@my.jcu.edu.au>
2025-08-02 21:57:00 +0800

    Set up input loop for user search

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,2 +1,7 @@
 import wikipedia
 
+while True:
+    # Prompt the user for a page title or search phrase
+    search_input = input("Enter a page title or search phrase (or leave blank to exit): ")
+    if not search_input:
+        break  # Exit the loop if the input is blank
