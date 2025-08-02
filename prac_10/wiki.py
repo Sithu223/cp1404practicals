f3041e63e6921a9f09eeca9db69b36677b129676
SithuHein <sithuhein@my.jcu.edu.au>
2025-08-02 22:03:58 +0800

    Complete wiki.py exercise with the same output like the sample output

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -4,6 +4,7 @@ while True:
     # Prompt the user for a page title or search phrase
     search_input = input("Enter a page title or search phrase (or leave blank to exit): ")
     if not search_input:
+        print("Thank you.")
         break  # Exit the loop if the input is blank
 
     try:
